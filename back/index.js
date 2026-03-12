9d831f2e91f30866391fb233cc168ea76a1c2b01
nasirulla <rehmanansirulla1124@gmail.com>
2026-03-12 06:13:14 +0530

    deploy

--- back/index.js
+++ back/index.js
@@ -15,10 +15,19 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://twiter-1-rkwj.onrender.com"
+    ],
     credentials: true,
   }),
 );
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
 
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
